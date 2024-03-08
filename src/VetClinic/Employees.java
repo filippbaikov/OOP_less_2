831479355eea8c6aafb0e84ed869bf93b0d63fb8
filipp <zeliboba0@bk.ru>
2024-03-08 21:42:28 +0300

    a

---
+++ src/VetClinic/Employees.java
@@ -0,0 +1,15 @@
+package VetClinic;
+
+public class Employees {
+    public String name;
+    public   String surname;
+    public float salary;
+    public String specialization;
+
+    public Employees(String name, String surname, float salary, String specialization) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+        this.specialization = specialization;
+    }
+}
